ffa47540437db502783a039896d4680498aa084c
LittleFish-Coder <adrianyu890317@gmail.com>
2024-09-10 20:56:38 +0800

    change try except

--- main.py
+++ main.py
@@ -40,15 +40,14 @@ def get_info():
 
 def track_price(url: str):
 
-    # request
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    price = soup.find("span", {"class": "a-price-whole"}).get_text()
-    print(f"Time: {time.ctime()}, Price: {price}")
-
     try:
+        # request
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        price = soup.find("span", {"class": "a-price-whole"}).get_text()
+        print(f"Time: {time.ctime()}, Price: {price}")
         price = float(price.replace(",", ""))
 
         if price < 60000:
@@ -60,7 +59,6 @@ def track_price(url: str):
 
     except ValueError:
         print("Price is not available.")
-        return
 
 
 def job():
